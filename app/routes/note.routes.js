97caa9e4ac1b7e7e603fac8e0a2ab2002053635a
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-02-27 15:38:03 -0800

    set up mongodb and routes files

---
+++ app/routes/note.routes.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+
+  var notes = require('../controllers/note.controller.js')
+
+  // Create a new Note
+  app.post('/notes', notes.create)
+
+  // Retrieve all Notes
+  app.get('/notes', notes.create)
+
+  // Retrieve a single Note with nodeId
+  app.get('/notes/:noteId', notes.findOne)
+
+  // Update a Note with noteId
+  app.put('/notes/:noteId', notes.update)
+
+  // Delete a Note with noteId
+  app.delete('/notes/:noteId', notes.delete)
+}
